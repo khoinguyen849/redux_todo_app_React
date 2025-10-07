dfc40c14aa6666828d148610458a7de11e4d1a9d
khoii3110 <khoinguyen.dev3110@gmail.com>
2025-10-08 01:31:11 +0700

    replace useDispatch AddTodo.jsx with connect()

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,34 +1,33 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import { addTodo } from "../actions/todoActions";
 import "./AddTodo.css";
 
-const AddTodo = () => {
-    const [inputValue, setInputValue] = useState("");
-    const dispatch = useDispatch();
+const AddTodo = ({ addTodo }) => {
+  const [inputValue, setInputValue] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (inputValue.trim()) {
-            dispatch(addTodo(inputValue.trim()));
-            setInputValue("");
-        }
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      addTodo(inputValue.trim());
+      setInputValue("");
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className="add-todo-form">
-            <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                placeholder="Add a new todo..."
-                className="add-todo-input"
-            />
-            <button type="submit" className="add-todo-button">
-                Add Todo
-            </button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit} className="add-todo-form">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new todo..."
+        className="add-todo-input"
+      />
+      <button type="submit" className="add-todo-button">
+        Add Todo
+      </button>
+    </form>
+  );
 };
 
-export default AddTodo;
+export default connect(null, { addTodo })(AddTodo);
